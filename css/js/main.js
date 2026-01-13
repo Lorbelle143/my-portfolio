60809573be237c9df51bdbbb0b87e26c86e942d3
Lorbelle143 <lorbelleganzan@gmail.com>
2026-01-13 12:19:44 +0800

    updated js

--- css/js/main.js
+++ css/js/main.js
@@ -1,6 +1,19 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const sections = document.querySelectorAll("section");
+const links = document.querySelectorAll("nav a");
 
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("active");
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        if (scrollY >= section.offsetTop - 120) {
+            current = section.id;
+        }
+    });
+
+    links.forEach(link => {
+        link.classList.remove("active");
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
 });

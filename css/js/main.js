53afc7f038e075dfe4429298cb4e2279d9a9bbbe
Lorbelle143 <lorbelleganzan@gmail.com>
2026-01-13 12:58:49 +0800

    finnal

--- css/js/main.js
+++ css/js/main.js
@@ -1,19 +1,5 @@
-const sections = document.querySelectorAll("section");
-const links = document.querySelectorAll("nav a");
-
-window.addEventListener("scroll", () => {
-    let current = "";
-
-    sections.forEach(section => {
-        if (scrollY >= section.offsetTop - 120) {
-            current = section.id;
-        }
-    });
-
-    links.forEach(link => {
-        link.classList.remove("active");
-        if (link.getAttribute("href") === "#" + current) {
-            link.classList.add("active");
-        }
-    });
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Message sent successfully!');
+  this.reset();
 });
